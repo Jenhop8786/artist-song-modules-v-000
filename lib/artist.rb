91049a8f9319b6d94c3609e0e30453305c9125d5
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-17 22:29:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,15 +26,12 @@ class Artist
   end
 
 
-
-
-
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
-  def add_songs(songs)
+  #def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
