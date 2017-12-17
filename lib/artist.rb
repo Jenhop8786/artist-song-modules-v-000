2b3c11c18aec2c84933525049401d22acdde5ee0
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-17 22:22:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,50 +1,50 @@
 require 'pry'
 
-class Artist 
+class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
-  attr_accessor :name 
-  attr_reader :songs 
+  attr_accessor :name
+  attr_reader :songs
 
   @@artists = []
 
   def self.find_by_name(name)
     @@artists.detect.{|a| a.name == name}
-  end 
+  end
 
-  def initialize 
-    @@artists << self 
-    super 
+  def initialize
+    @@artists << self
+    super
     @songs = []
-  end 
+  end
 
-  def self.all 
+  def self.all
     @@artists
-  end 
+  end
 
-  def self.reset_all 
-    self.all.clear 
-  end 
+  def self.reset_all
+    self.all.clear
+  end
 
-  def self.count 
-    @@artists.count 
-  end 
+  def self.count
+    @@artists.count
+  end
 
   def add_song(song)
-    @songs << song 
+    @songs << song
     song.artists = self
-  end 
+  end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end 
+  end
 
-  def to_param 
+  def to_param
     name.downcase.gsub(' ', '-')
-  end 
+  end
 
-end 
+end
     
